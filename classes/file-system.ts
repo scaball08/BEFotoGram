589f63264bd3566ea18e77dc20a47b89274efb99
scaball08 <scaballeros08@gmail.com>
2020-08-18 02:49:20 -0500

    foto default

--- classes/file-system.ts
+++ classes/file-system.ts
@@ -98,6 +98,7 @@ export class FileSystem {
       'posts',
       img
     );
+    
 
     return pathFoto;
   }
--- dist/classes/token.js
+++ dist/classes/token.js
@@ -4,29 +4,26 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
-let Token = /** @class */ (() => {
-    class Token {
-        constructor() { }
-        static getJwtToken(payload) {
-            return jsonwebtoken_1.default.sign({
-                usuario: payload,
-            }, this.seed, { expiresIn: this.caducidad });
-        }
-        static comprobarToken(userToken) {
-            return new Promise((resolve, reject) => {
-                jsonwebtoken_1.default.verify(userToken, this.seed, (err, decoded) => {
-                    if (err) {
-                        reject();
-                    }
-                    else {
-                        resolve(decoded);
-                    }
-                });
-            });
-        }
+class Token {
+    constructor() { }
+    static getJwtToken(payload) {
+        return jsonwebtoken_1.default.sign({
+            usuario: payload,
+        }, this.seed, { expiresIn: this.caducidad });
     }
-    Token.seed = 'este-es-el-seed-de-mi-app-secreto';
-    Token.caducidad = '30d';
-    return Token;
-})();
+    static comprobarToken(userToken) {
+        return new Promise((resolve, reject) => {
+            jsonwebtoken_1.default.verify(userToken, this.seed, (err, decoded) => {
+                if (err) {
+                    reject();
+                }
+                else {
+                    resolve(decoded);
+                }
+            });
+        });
+    }
+}
 exports.default = Token;
+Token.seed = 'este-es-el-seed-de-mi-app-secreto';
+Token.caducidad = '30d';
